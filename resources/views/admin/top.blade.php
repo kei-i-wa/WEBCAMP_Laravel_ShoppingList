3a93adefe444877ab32daf71452475ed051e734a
keiiwasaki <kei.kumarg@gmail.com>
2022-03-26 05:23:43 +0000

    FINISH 課題最終確認

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -3,10 +3,5 @@
 @section('title')@endsection
 
 @section('contents')
-    <menu label="リンク">
-        <a href="./top">管理画面TOP</a><br>
-        <a href="./user/list">ユーザ一覧</a><br>
-        <a href="./logout">ログアウト</a><br>
-    </menu>
     <h1>管理画面</h1>
 @endsection
